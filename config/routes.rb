6b9b073909c5b4354cc53523ba0498c711ffb65a
kasi-iyo <snakeman143@gmail.com>
2020-06-05 17:25:30 +0900

    Post、User、Favoriteのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'posts#index'
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
