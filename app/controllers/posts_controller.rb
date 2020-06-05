651d40422faf104e1766ae6db6d311b5faa2a085
kasi-iyo <snakeman143@gmail.com>
2020-06-05 19:47:25 +0900

    postsコントローラ編集。ユーザーとアソシエーションされた投稿の作成。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to @post
+    end
+  end
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :content)
   end
 end
