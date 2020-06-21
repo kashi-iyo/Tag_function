af8fe4a393ee25f54a188ca05d6e4b038df7fba1
kasi-iyo <snakeman143@gmail.com>
2020-06-21 17:27:18 +0900

    yarnにrails-ujsパッケージの追加。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,4 @@ console.log('Hello World from Webpacker')
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+require("./rails-ujs.js")
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users
+  resources :users, only: [:index, :show]
 
 
   #検索結果をpostsコントローラのsearchアクションへ送信
