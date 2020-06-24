95439eb4381abfa7eb03537073d0647a1f8dd4ff
kasi-iyo <snakeman143@gmail.com>
2020-06-24 17:38:55 +0900

    ツイートの投稿用フォームの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,27 @@
 class TweetsController < ApplicationController
+
+  before_action :authenticate_user!
+  
+  def index
+    @tweet = current_user.tweets.build
+  end
+
+  def create
+    @tweet = current_user.tweets.new(tweet_params) if @tweet.present?
+    Rails.logger.debug "@tweet : #{@tweet.inspect}"
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render tweets_path
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
+  # default_scope -> { order(created_at: :desc) }
 end
